8480d7a8afb5bf500c1ca5efaf496b87d9796053
PeerChristensen <hr.pchristensen@gmail.com>
2020-02-08 00:30:47 +0100

    word2vec

--- h2o_word2vec.R
+++ h2o_word2vec.R
@@ -9,41 +9,58 @@ library(tidytext)
 
 h2o.init()
 
-w2v_predict <- function(job.title, w2v, gbm) {
-  words <- tokenize(as.character(as.h2o(job.title)))
-  job.title.vec <- h2o.transform(w2v, words, aggregate_method = "AVERAGE")
-  h2o.predict(gbm, job.title.vec)
-}
-
 
 df <- read_excel("NPS_mapping.xlsx") %>%
-  select(Tekst, Score, Tag = `NPS TAG`) %>%
+  select(Tekst, Tag = `NPS TAG`) %>%
   mutate(Tag = str_replace_all(Tag, c("Unknow" = "Unknown","Unknownn" = "Unknown",
                                       "e-boger" = "e-bøger"))) %>%
   mutate(Tag = str_replace(Tag, " ","")) %>%
   mutate(Tag = str_to_lower(Tag),
          Tekst = str_replace_all(Tekst, "[[:punct:]]", "")) %>%
-  filter(!is.na(Tag))
+  filter(!is.na(Tag)) 
 
 #### tokenize
-my_stopwords <- str_remove(stopwords::stopwords("danish"),"ikke")
+my_stopwords <- c(stopwords::stopwords("danish"),"ikke")
 
 df <- df %>% 
   mutate(doc_id = row_number()) %>%
   unnest_tokens(word,Tekst) %>%
   mutate(word = tm::removeWords(word, c(my_stopwords,"saxo"))) %>%
-  filter(word != "")
+  filter(word != "") %>%
+  mutate(Tag = factor(Tag))
 
-word <- as.h2o(df$word)
+df_hf <- as.h2o(df)
+
+word <- h2o.tokenize(df_hf$word,"\\\\W+")
+word <- tokenize(df_hf$word)
 
 # word2vec model
 w2v.model <- h2o.word2vec(word, sent_sample_rate = 0, epochs = 10)
 
-job.title.vecs <- h2o.transform(w2v.model, words, aggregate_method = "AVERAGE")
-valid.job.titles <- ! is.na(job.title.vecs$C1)
+tags_vecs <- h2o.transform(w2v.model, word, aggregate_method = "AVERAGE")
+valid_tags_vecs <- ! is.na(tags_vecs$C1)
 
-data <- h2o.cbind(job.titles[valid.job.titles, "category"], job.title.vecs[valid.job.titles, ])
+data <- h2o.cbind(df_hf[valid_tags_vecs, "Tag"], tags_vecs[valid_tags_vecs, ])
 data.split <- h2o.splitFrame(data, ratios = 0.8)
 
-gbm.model <- h2o.gbm(x = names(job.title.vecs), y = "category",
-                     training_frame = data.split[[1]], validation_frame = data.split[[2]])
+aml <- h2o.automl(x = names(tags_vecs), y = "Tag",
+                     training_frame = data.split[[1]], 
+               validation_frame = data.split[[2]],
+               max_runtime_secs = 500,
+               balance_classes = F)
+
+aml@leaderboard
+
+perf <- h2o.performance(aml@leader,valid=T)
+
+perf@metrics$cm
+
+pred <- predict(aml@leader,data.split[[2]])
+
+actual <- data.split[[2]]$Tag %>% as.vector()
+pred <- as_tibble(pred) %>%
+  mutate(actual = actual) %>%
+  select(actual,predict)
+
+table(pred$actual==pred$predict)
+prop.table(table(pred$actual==pred$predict))
